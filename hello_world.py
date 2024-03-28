a712869e85f8e9edc396d0e1419f6ea316014fb1
Alexandre <alexandrej.lima.mendes@gmail.com>
2024-03-28 17:58:15 -0300

    Define new classes, Add a xml parser

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,44 @@
 import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
+import xml.etree.ElementTree as ElementTree
+
+
+class Ponto:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def print(self):
+        print("x: " + str(self.x), ", y: " + str(self.y))
+
+
+class Coordenadas:
+    def __init__(self, minimo: Ponto, maximo: Ponto):
+        self.minimo = minimo
+        self.maximo = maximo
+
+    def print(self):
+        print("minimo: ")
+        self.minimo.print()
+        print("maximo: ")
+        self.maximo.print()
+
+
+class Reta:
+    def __init__(self, a: Ponto, b: Ponto):
+        self.a = a
+        self.b = b
+
+    def print(self):
+        self.a.print()
+        self.b.print()
+
+
+class Poligono:
+
+    def __init__(self, *pontos: Ponto):
+        self.pontos = list(pontos)
 
 
 class MyWidget(QtWidgets.QWidget):
@@ -26,11 +64,35 @@ class MyWidget(QtWidgets.QWidget):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    # app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+    # widget = MyWidget()
+    # widget.resize(800, 600)
+    # widget.show()
 
-    sys.exit(app.exec())
+    # root = entrada.getroot()
+    # print(root.tag, root.attrib)
+    #    for child in root:
+    #        print(child.tag, child.attrib)
+    entrada = ElementTree.parse('docs/entrada.xml')
+    root = entrada.getroot()
+    vp_min = Ponto(*root.find("./viewport/vpmin").attrib.values())
+    vp_max = Ponto(*root.find("./viewport/vpmax").attrib.values())
+    viewport = Coordenadas(vp_min, vp_max)
+    wmin = Ponto(*root.find("./window/wmin").attrib.values())
+    wmax = Ponto(*root.find("./window/wmax").attrib.values())
+    window = Coordenadas(wmin, wmax)
+    window.print()
+    viewport.print()
+    pontos = [Ponto(*ponto.attrib.values()) for ponto in root.findall("./ponto")]
+    for ponto in pontos:
+        ponto.print()
 
+    retas = [Reta(Ponto(*ponto.attrib.values()), Ponto(*ponto.attrib.values())) for reta in root.findall("./reta") for
+             ponto
+             in reta]
+    print("Retas:")
+    for reta in retas:
+        reta.print()
+
+    # sys.exit(app.exec())
