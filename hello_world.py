092e1f7b12dc663f33aa7ae68e94234b8cc0ecff
Hugo Leonardo <hugoleonardomachado@hotmail.com>
2024-03-29 17:41:38 -0300

    fix: ajustes gerais

--- hello_world.py
+++ hello_world.py
@@ -1,45 +1,98 @@
-import sys
+from abc import ABC
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
 import xml.etree.ElementTree as ElementTree
 
-
 class Ponto:
-    def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
+    def __init__(self, x: float, y: float) -> None:
+        self._x = float(x)
+        self._y = float(y)
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
 
     def print(self):
-        print("x: " + str(self.x), ", y: " + str(self.y))
+        print("x: " + str(self._x), "y: " + str(self._y), sep=", ")
 
+class Retangulo(ABC):
+    def __init__(self, p_minimo: Ponto, p_maximo: Ponto):
+        if not all(isinstance(p, Ponto) for p in (p_minimo, p_maximo)):
+            raise TypeError("As coordenadas devem ser um Ponto")
+        self._p_minimo = p_minimo
+        self._p_maximo = p_maximo
 
-class Coordenadas:
-    def __init__(self, minimo: Ponto, maximo: Ponto):
-        self.minimo = minimo
-        self.maximo = maximo
+    @property
+    def p_minimo(self):
+        return self._p_minimo
+
+    @property
+    def p_maximo(self):
+        return self._p_maximo
+
+    def comprimento(self):
+        return self._p_maximo.x - self._p_minimo.x
+
+    def altura(self):
+        return self._p_maximo.y - self._p_minimo.y
 
     def print(self):
-        print("minimo: ")
-        self.minimo.print()
-        print("maximo: ")
-        self.maximo.print()
+        print("Ponto mínimo:", end=" ")
+        self._p_minimo.print()
+        print("Ponto máximo:", end=" ")
+        self._p_maximo.print()
+        print("Dimensão:", end=" ")
+        print(self.comprimento(), self.altura(), sep="x")
 
+class Window(Retangulo):
+    def print(self):
+        print("---- Window ----")
+        super().print()
+
+class Viewport(Retangulo):
+    def print(self):
+        print("---- Viewport ----")
+        super().print()
 
 class Reta:
     def __init__(self, a: Ponto, b: Ponto):
-        self.a = a
-        self.b = b
+        if not all(isinstance(p, Ponto) for p in (a, b)):
+            raise TypeError("As coordenadas devem ser um Ponto")
+        self._a = a
+        self._b = b
+
+    @property
+    def a(self):
+        return self._a
+
+    @property
+    def b(self):
+        return self._b
+
+    def tamanho(self):
+        dx = self._b.x - self._a.x
+        dy = self._b.y - self._a.y
+        return (dx**2 + dy**2)**0.5
 
     def print(self):
-        self.a.print()
-        self.b.print()
-
+        print("---- Reta ----")
+        self._a.print()
+        self._b.print()
+        print("Tamanho: ", self.tamanho())
 
 class Poligono:
-
     def __init__(self, *pontos: Ponto):
-        self.pontos = list(pontos)
+        if not all(isinstance(p, Ponto) for p in pontos):
+            raise TypeError("As coordenadas devem ser um Ponto")
+        self._pontos = list(pontos)
 
+    @property
+    def pontos(self):
+        return self._pontos
 
 class MyWidget(QtWidgets.QWidget):
 
@@ -64,35 +117,35 @@ class MyWidget(QtWidgets.QWidget):
 
 
 if __name__ == "__main__":
-    # app = QtWidgets.QApplication([])
+    dados = ElementTree.parse('docs/entrada.xml').getroot()
 
-    # widget = MyWidget()
-    # widget.resize(800, 600)
-    # widget.show()
-
-    # root = entrada.getroot()
-    # print(root.tag, root.attrib)
-    #    for child in root:
-    #        print(child.tag, child.attrib)
-    entrada = ElementTree.parse('docs/entrada.xml')
-    root = entrada.getroot()
-    vp_min = Ponto(*root.find("./viewport/vpmin").attrib.values())
-    vp_max = Ponto(*root.find("./viewport/vpmax").attrib.values())
-    viewport = Coordenadas(vp_min, vp_max)
-    wmin = Ponto(*root.find("./window/wmin").attrib.values())
-    wmax = Ponto(*root.find("./window/wmax").attrib.values())
-    window = Coordenadas(wmin, wmax)
+    # ---- Window ----
+    wmin = Ponto(*dados.find("window/wmin").attrib.values())
+    wmax = Ponto(*dados.find("window/wmax").attrib.values())
+    window = Window(wmin, wmax)
     window.print()
+
+    # ---- Viewport ----
+    vp_min = Ponto(*dados.find("viewport/vpmin").attrib.values())
+    vp_max = Ponto(*dados.find("viewport/vpmax").attrib.values())
+    viewport = Viewport(vp_min, vp_max)
     viewport.print()
-    pontos = [Ponto(*ponto.attrib.values()) for ponto in root.findall("./ponto")]
+
+    # ---- Pontos ----
+    pontos = [Ponto(*ponto.attrib.values()) for ponto in dados.findall("ponto")]
     for ponto in pontos:
         ponto.print()
 
-    retas = [Reta(Ponto(*ponto.attrib.values()), Ponto(*ponto.attrib.values())) for reta in root.findall("./reta") for
-             ponto
-             in reta]
-    print("Retas:")
+    # ---- Retas ----
+    retas = [
+        Reta(Ponto(*list(reta)[0].attrib.values()), Ponto(*list(reta)[1].attrib.values()))
+        for reta in dados.findall("reta")
+    ]
     for reta in retas:
         reta.print()
 
-    # sys.exit(app.exec())
+    # app = QtWidgets.QApplication([])
+
+        # widget = MyWidget()
+        # widget.resize(800, 600)
+        # widget.show()
