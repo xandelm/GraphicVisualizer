e55ca97940256c0de0fc2d97894914ee458799d8
xandejlm <alexandrej.lima.mendes@gmail.com>
2024-03-30 20:41:27 -0300

    fix: run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 python -m venv venv
-#source venv/Scripts/activate #for linux
-.\venv\Scripts\activate #for windows
+source venv/Scripts/activate #for linux/bash
+#.\venv\Scripts\activate #for windows
 pip install -r requirements.txt
 python main.py
