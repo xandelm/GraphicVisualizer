5cc0884145856255b66373e134bfa2a1e636f174
xandejlm <alexandrej.lima.mendes@gmail.com>
2024-08-16 16:02:01 -0300

    feat: adiciona inserção de pontos e retas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from abc import ABC
 from PySide6.QtCore import Qt, QPointF
-from PySide6.QtGui import QPolygonF, QTransform
-from PySide6.QtWidgets import  QStyle, QGridLayout, QVBoxLayout, QWidget,QApplication, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsPolygonItem, QMainWindow, QPushButton, QDockWidget
+from PySide6.QtGui import QPolygonF
+from PySide6.QtWidgets import QLabel, QMessageBox, QLineEdit, QDialog, QStyle, QGridLayout, QVBoxLayout, QWidget,QApplication, QGraphicsView, QGraphicsScene, QGraphicsEllipseItem, QGraphicsLineItem, QGraphicsPolygonItem, QMainWindow, QPushButton, QDockWidget
 import xml.etree.ElementTree as ElementTree
 import sys, math
 
@@ -141,6 +141,114 @@ class Poligono:
         for ponto in self._pontos:
             ponto.print()
 
+
+class PointDialog(QDialog):
+    def __init__(self, x_value: float = '' , y_value: float = ''):
+        super().__init__()
+        self.setWindowTitle("Inserir Ponto")
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(QLabel("Insira o valor X:"))
+        self.x_input = QLineEdit()
+        self.x_input.setText(str(x_value))
+        layout.addWidget(self.x_input)
+
+        layout.addWidget(QLabel("Insira o valor Y:"))
+        self.y_input = QLineEdit()
+        self.y_input.setText(str(y_value))
+        layout.addWidget(self.y_input)
+
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.accept)
+        layout.addWidget(ok_button)
+
+        self.setLayout(layout)
+
+    def get_values(self) -> Ponto:
+        x_text = self.x_input.text()
+        y_text = self.y_input.text()
+        if not x_text or not y_text:
+            warning = warningMessage("Erro", "Os campos de entrada não podem estar vazios")
+            warning.exec()
+            raise ValueError("Os campos de entrada não podem estar vazios")
+        try:
+            x = float(self.x_input.text())
+            y = float(self.y_input.text())
+        except:
+            warning = warningMessage("Erro", "Os valores inseridos devem ser numéricos")
+            warning.exec()
+            raise ValueError("Os valores inseridos devem ser numéricos")
+        return Ponto(x, y)
+
+class warningMessage(QMessageBox):
+    def __init__(self, windowTitle: str, text: str):
+        super().__init__()
+        self.setWindowTitle(windowTitle)
+        self.setText(text)
+        self.setIcon(QMessageBox.Warning)
+        self.setStandardButtons(QMessageBox.Ok)
+
+class LineDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Inserir Reta")
+
+        layout = QVBoxLayout()
+
+        self.insert_point_a_button = QPushButton()
+        self.insert_point_a_button.setText("Inserir Ponto A")
+        self.insert_point_a_button.clicked.connect(lambda: self.open_point_dialog("A"))
+        layout.addWidget(self.insert_point_a_button)
+
+        self.insert_point_b_button = QPushButton("Inserir Ponto B")
+        self.insert_point_b_button.clicked.connect(lambda: self.open_point_dialog("B"))
+        layout.addWidget(self.insert_point_b_button)
+
+        ok_button = QPushButton("OK")
+        ok_button.clicked.connect(self.accept)
+        layout.addWidget(ok_button)
+
+        self.setLayout(layout)
+
+    def open_point_dialog(self, point_id: str):
+        dialog = PointDialog()
+        if dialog.exec() == QDialog.Accepted:
+            try:
+                point = dialog.get_values()
+                if point_id == "A":
+                    self.point_a = point
+                    self.insert_point_a_button.setText("Alterar Ponto A")
+                elif point_id == "B":
+                    self.point_b = point
+                    self.insert_point_b_button.setText("Alterar Ponto B")
+                else:
+                    raise ValueError("Identificador de ponto inválido")
+            except ValueError as e:
+                warning = warningMessage("Erro", str(e))
+                warning.exec()
+                self.reject()
+
+    
+    def get_line(self) -> Reta:
+        if hasattr(self, 'point_a') and hasattr(self, 'point_b'):
+            return Reta(self.point_a, self.point_b)
+        else:
+            warning = warningMessage("Erro", "Pontos A e B devem ser inseridos")
+            warning.exec()
+            raise ValueError("Pontos A e B devem ser inseridos")
+
+""""TODO: Implementar a classe PoligonoDialog
+class PoligonoDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.windowTitle("Inserir Poligono")
+
+        layout = QVBoxLayout()
+        
+"""
+    
 class ViewportWindow(QWidget):
     def __init__(self, viewport, pontos, retas, poligonos):
         super().__init__()
@@ -334,12 +442,20 @@ class MainWindow(QMainWindow):
         self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock_widget)
 
         self.setCentralWidget(self.viewport)
+
         menu_bar = self.menuBar()
         insert_menu = menu_bar.addMenu("Inserir")
-        insert_menu.addAction("Ponto")
-        insert_menu.addAction("Reta")
-        insert_menu.addAction("Poligono")
+
+        insert_point_action = insert_menu.addAction("Ponto")
+        insert_line_action = insert_menu.addAction("Reta")
+        insert_poligon_action = insert_menu.addAction("Poligono")
+        
         export_menu = menu_bar.addMenu("Exportar")
+
+        insert_point_action.triggered.connect(self.open_point_dialog)
+        insert_line_action.triggered.connect(self.open_line_dialog)
+        #TODO: Implementar método para abrir o diálogo de polígonos
+        #insert_poligon_action.triggered.connect(self.open_poligono_dialog)
 
         right_button.clicked.connect(self.viewport.move_right)
         left_button.clicked.connect(self.viewport.move_left)
@@ -349,6 +465,24 @@ class MainWindow(QMainWindow):
         rotate_right_button.clicked.connect(lambda: self.viewport.rotate_objects(10))
         zoom_in_button.clicked.connect(self.viewport.zoom_in)
         zoom_out_button.clicked.connect(self.viewport.zoom_out)
+
+    def open_point_dialog(self):
+        dialog = PointDialog()
+        if dialog.exec() == QDialog.Accepted:
+            point = dialog.get_values()
+            self.viewport.pontos.append(point)
+            self.viewport.update_scene()
+
+    def open_line_dialog(self):
+        dialog = LineDialog()
+        if dialog.exec() == QDialog.Accepted:
+            line = dialog.get_line()
+            self.viewport.retas.append(line)
+            self.viewport.update_scene()
+
+    #TODO: Implementar método para abrir o diálogo de polígonos
+    #def open_poligono_dialog(self):
+        
 
 def transformar_pontos_viewport(window: Window, viewport: Viewport, pontos: list[Ponto]) -> list[Ponto]:
     pontos_vp = []
